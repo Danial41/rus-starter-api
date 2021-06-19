df25a4d048a85325e0cd3fe9c895a7ee0eeb4a4e
Denis <prorutop@yandex.ru>
2021-06-19 17:33:05 +0300

    Did some stupid stuff

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :users
   post '/login', to: 'users#login'
   post '/join_team/:id', to: 'participants#join_team'
-  get  '/pending_participants', to: 'participants#pending_participants'
+  get  '/pending_participants/:id', to: 'participants#pending_participants'
   put '/edit', to: 'profiles#update'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
