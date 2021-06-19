3e0b8bc4329a6782095e042bd9b402b07472e5b8
Denis <prorutop@yandex.ru>
2021-06-19 11:03:50 +0300

    Add image to serializer

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,5 +1,8 @@
 class ProjectSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description
+  attributes :id, :title, :description, :image
   has_one :user
   
+  def image
+    object.image.service_url if object.image.attached?
+  end
 end
